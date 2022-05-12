0e7132cf8b937a79b2a472dc2318103dccaaa148
Annie Ma <anniema@Annies-MacBook-Pro.local>
2022-05-12 17:05:26 -0400

    JS updated

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const url = "https://api.quotable.io/random"
+
+const $quote = $('#content'); 
+const $author = $('#author'); 
+const $getQuote = $('#getQuote'); 
+
+//events 
+//Button switches color during hover 
+$getQuote.on('click', getQuote); 
+$('#getQuote').hover(function(){
+    $(this).css('background-color', 'aquamarine'); 
+}); 
+$('#getQuote').mouseleave(function(){
+    $(this).css('background-color', 'lightgreen'); 
+})
+
+
+//functions 
+function getQuote(event){
+    event.preventDefault()
+
+    $.ajax(url).then(function(data){
+        console.log('quote generated')
+
+        $quote.text(data.content)
+        $author.text(data.author)
+
+    }, function(error){
+        console.log('something is wrong')
+        console.log(error)
+    })
+}
